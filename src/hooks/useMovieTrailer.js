40c4d684b88c3634f4d0da93d50cc738086186fd
Piyush Pandey <piyush2003pandey@gmail.com>
2025-03-10 21:45:05 +0530

    added movie trailer and structured the main container

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  const getMoviesVideos = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filteredData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const trailer =
+      filteredData.length === 0 ? filteredData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    getMoviesVideos();
+  }, []);
+};
+
+export default useMovieTrailer;
