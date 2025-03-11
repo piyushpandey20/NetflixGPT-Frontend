e3570c7856424bba80c33a5f3b1e8383b6450ef1
Piyush Pandey <piyush2003pandey@gmail.com>
2025-03-11 17:23:26 +0530

    secondary container completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="w-full absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2 justify-between items-center">
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,14 +3,14 @@ import { FaCircleInfo, FaPlay } from "react-icons/fa6";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-black">
-      <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
-      <div className="flex items-center">
-        <button className="bg-white text-black p-4 px-12 text-xl rounded-lg hover:bg-opacity-80 flex justify-between items-center gap-2">
+    <div className="w-full aspect-video pt-[15%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
+      <p className="hidden md:inline-block py-6 text-lg w-1/3">{overview}</p>
+      <div className="my-4 md:m-0 flex items-center">
+        <button className="bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl rounded-lg hover:bg-opacity-80 flex justify-between items-center gap-2">
           <FaPlay /> Play
         </button>
-        <button className="bg-gray-500 p-4 px-12 mx-2 text-xl bg-opacity-50 rounded-lg flex justify-between items-center gap-2">
+        <button className="bg-gray-500 p-4 px-12 mx-2 text-xl bg-opacity-50 rounded-lg hidden md:flex justify-between items-center gap-2">
           <FaCircleInfo /> More Info
         </button>
       </div>
